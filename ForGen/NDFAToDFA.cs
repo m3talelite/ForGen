0fb4fdb09e5881f035e230ad6f6b5ff3646f6693
joris mathijssen <jorismathijssen@gmail.com>
2015-06-17 19:06:27 +0200

    Added NDFAtoDFA start

---
+++ ForGen/NDFAToDFA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForGen
+{
+	public class NDFAToDFA
+	{
+		public NDFAToDFA(Automata<String> NDFA)
+		{
+			converter (NDFA);
+		}
+
+		public Automata<String> converter(Automata<String> NDFA)
+		{
+			if (!NDFA.isDFA ()) {
+				char [] alphabet = {'a', 'b'};
+				Automata<String> dfa = new Automata<String>(alphabet);
+				return dfa;
+			} else {
+				//NDFA is a DFA so return that!
+				return NDFA;
+			}
+		}
+	}
+}
+
