6f07b0a29a47bf36b6e012ed226cad79976843af
Guus Beckett <guus@reupload.nl>
2015-07-05 17:26:40 +0200

    Fixed regex not printing latex proof

--- ForGen/LaTeXGenerator.cs
+++ ForGen/LaTeXGenerator.cs
@@ -104,14 +104,14 @@ namespace ForGen
 		private String exerciseNDFAToDFA()
 		{
 			//NDFA_FOR_NDFA_TO_DFA
-			String result = "\\section{NDFA naar DFA}\nZet de onderstaande NDFA om naar een DFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
+			String result = "\\section{NDFA naar DFA}\nZet de onderstaande NDFA om naar een DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseRegexToDFA(RegularExpression regex)
 		{
 			String result = "\\section{Reguliere expressie}\nGeef de DFA van de onderstaande reguliere expressie:" +
-			    regex.ToString()
+				regexStringToLatexString(regex.ToString())
 				+"\\clearpage";
 			return result;
 		}
@@ -119,7 +119,7 @@ namespace ForGen
 		private String exerciseRegexToNDFA(RegularExpression regex)
 		{
 			String result = "\\section{Reguliere expressie}\nGeef de NDFA van de onderstaande reguliere expressie:" +
-			    regex.ToString()
+				regexStringToLatexString(regex.ToString())
 				+"\\clearpage";
 			return result;
 		}
@@ -127,103 +127,114 @@ namespace ForGen
 		private String exerciseMinimaliseDFA()
 		{
 			//DFA_FOR_MINIMALISE_DFA
-			String result = "\\section{Minimalisatie}\nMinimaliseer de onderstaande DFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
+			String result = "\\section{Minimalisatie}\nMinimaliseer de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseReverseDFA()
 		{
 			//DFA_FOR_DFA_TO_INVERSE_DFA
-			String result = "\\section{Inverse}\nGeef de inverse (het omgekeerde) van de onderstaande DFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
+			String result = "\\section{Inverse}\nGeef de inverse (het omgekeerde) van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseDFAToGrammar()
 		{
 			//DFA_FOR_DFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande DFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseNDFAToGrammar()
 		{
 			//NDFA_FOR_NDFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande NDFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande NDFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseDFAToRegex()
 		{
 			//DFA_FOR_DFA_TO_REGEX
-			String result = "\\section{Omzetten naar regluliere expressie}\nGeef de regex van de onderstaande DFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_REGEX}\n\n\\clearpage";
+			String result = "\\section{Omzetten naar regluliere expressie}\nGeef de regex van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_REGEX}\n\n\\clearpage";
 			return result;
 		}
 
 		private String exerciseNDFAToRegex()
 		{
 			//NDFA_FOR_NDFA_TO_REGEX
-			String result = "\\section{Omzetten naar regluliere expressie}\nGeef de regex van de onderstaande NDFA:\n\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_REGEX}\n\n\\clearpage";
+			String result = "\\section{Omzetten naar regluliere expressie}\nGeef de regex van de onderstaande NDFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_REGEX}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerNDFAToDFA()
 		{
 			//DFA_FOR_NDFA_TO_DFA
-			String result = "\\section{NDFA naar DFA}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
+			String result = "\\section{NDFA naar DFA}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerRegexToDFA()
 		{
 			//DFA_FOR_REGEX_TO_DFA
-			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage";
+			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage";
 			return result;
 		}
 
 		private String answerRegexToNDFA()
 		{
 			//DFA_FOR_REGEX_TO_DFA
-			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage";
+			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage";
 			return result;
 		}
 
 		private String answerMinimaliseDFA()
 		{
 			//MINIMALISED_DFA_FOR_MINIMALISE_DFA
-			String result = "\\section{Minimalisatie}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {MINIMALISED_DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
+			String result = "\\section{Minimalisatie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {MINIMALISED_DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerReverseDFA()
 		{
 			//REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA
-			String result = "\\section{Inverse}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
+			String result = "\\section{Inverse}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerDFAToGrammar()
 		{
 			//DFA_GRAMMAR_FOR_DFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {DFA_GRAMMAR_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_GRAMMAR_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerNDFAToGrammar()
 		{
 			//NDFA_GRAMMAR_FOR_NDFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\n\\includegraphics[width=0.7\\textwidth,height=\\textheight,keepaspectratio] {NDFA_GRAMMAR_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_GRAMMAR_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage";
 			return result;
 		}
 
 		private String answerDFAToRegex(RegularExpression regex)
 		{
-			String result = "\\section{Omzetten naar reguliere expressie}"+regex.ToString();
+			String result = "\\section{Omzetten naar reguliere expressie}"+
+				regexStringToLatexString(regex.ToString());
 			return result;
 		}
 
 		private String answerNDFAToRegex(RegularExpression regex)
 		{
-			String result = "\\section{Omzetten naar reguliere expressie}"+regex.ToString();
+			String result = "\\section{Omzetten naar reguliere expressie}"+
+				regexStringToLatexString(regex.ToString());
+			return result;
+		}
+
+		private String regexStringToLatexString(String regexString)
+		{
+			String result = "";
+			result = regexString.Replace("|", "$\\arrowvert$");
+			result = result.Replace("+", "$^+$");
+			result = result.Replace("*", "$^*$");
 			return result;
 		}
 	}
