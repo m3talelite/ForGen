3b37829fdf7b90b85dbe5b2d5511c0241d8d7b61
Guus Beckett <guus@reupload.nl>
2015-07-05 13:52:14 +0200

    Added method stubs to Latex generator class

--- ForGen/LaTeXGenerator.cs
+++ ForGen/LaTeXGenerator.cs
@@ -13,6 +13,60 @@ namespace ForGen
 		{
 			
 		}
+
+		private String exerciseNDFAToDFA()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseRegexToDFA()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseRegexToNDFA()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseMinimaliseDFA()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseReverseDFA()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseDFAToGrammar()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseNDFAToGrammar()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseDFAToRegex()
+		{
+			String result = "";
+			return result;
+		}
+
+		private String exerciseNDFAToRegex()
+		{
+			String result = "";
+			return result;
+		}
 	}
 }
 
