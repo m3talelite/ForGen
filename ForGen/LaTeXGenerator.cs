3ecf598e65c849e34ea3e0045d1849c130ba1adf
Guus Beckett <guus@reupload.nl>
2015-07-06 22:35:11 +0200

    fixed some latex layout faults

--- ForGen/LaTeXGenerator.cs
+++ ForGen/LaTeXGenerator.cs
@@ -101,26 +101,26 @@ namespace ForGen
 
 		private String frontpageLatex()
 		{
-			String result = "\\documentclass[12pt]{article}\n\n\\pagestyle{empty}\n\\setcounter{secnumdepth}{2}\n%----------------------------------------------------------------------------------------\n%   Packages and configurations\n%----------------------------------------------------------------------------------------\n\\usepackage[dutch]{babel}\n\\usepackage{geometry} % Required to change the page size to A4\n\\geometry{a4paper} % Set the page size to be A4 as opposed to the default US Letter\n\\usepackage{chngpage}\n\\usepackage{fancyhdr} % Required for custom headers\n\\usepackage{extramarks} % Required for headers and footers\n\\usepackage{lastpage} % Required to determine the last page for the footer\n%-----------------1\n\\topmargin=0cm\n\\oddsidemargin=0cm\n\\textheight=22.0cm\n\\textwidth=16cm\n\\parindent=0cm\n\\parskip=0.15cm\n\\topskip=0truecm\n\\raggedbottom\n\\abovedisplayskip=3mm\n\\belowdisplayskip=3mm\n\\abovedisplayshortskip=0mm\n\\belowdisplayshortskip=2mm\n\\normalbaselineskip=12pt\n\\normalbaselines\n\\usepackage{wasysym}\n\\pagestyle{fancy}\n\\lhead{} % Top left header\n\\rhead{Oefententamen} % Top center header\n\\lfoot{Avans Hogeschool Breda} % Bottom left footer\n\\cfoot{} % Bottom center footer\n\\rfoot{Pagina\\ \\thepage} % Bottom right footer\n\\renewcommand\\headrulewidth{0.4pt} % Size of the header rule\n\\renewcommand\\footrulewidth{0.8pt} % Size of the footer rule\n\\usepackage{graphicx} % Required for including pictures\n\\usepackage{pdfpages}\n\\usepackage[font={footnotesize}]{caption}\n\\graphicspath{ {./Images/} } % We can store the generated images in a seperate folder without the hassle\n\\begin{document}\n\\begin{titlepage}\n\\newcommand{\\HRule}{\\rule{\\linewidth}{0.5mm}} % Defines a new command for the horizontal lines, change thickness here\n\n\\center % Center everything on the page\n\nAvans Hogeschool Breda\\\\% Include a department/university logo - this will require the graphicx package\n\\textsc{\\Large Technische Informatica}\\\\[0.5cm] % Major heading such as course name\n\\textsc{\\large Formele Methoden}\\\\[0.5cm] % Minor heading such as course title\n\\HRule \\\\[0.4cm]\n{ \\huge \\bfseries Oefententamen}\\\\[0.4cm] % Title of your document\n\\HRule \\\\[1.5cm]\n\n{\\large \\today}\\\\[3cm] % Date, change the \\today to a set date if you want to be precise\n\n\\vfill % Fill the rest of the page with whitespace\n\n\\end{titlepage}\n\n\\clearpage";
+			String result = "\\documentclass[12pt]{article}\n\n\\pagestyle{empty}\n\\setcounter{secnumdepth}{2}\n%----------------------------------------------------------------------------------------\n%   Packages and configurations\n%----------------------------------------------------------------------------------------\n\\usepackage[dutch]{babel}\n\\usepackage{geometry} % Required to change the page size to A4\n\\geometry{a4paper} % Set the page size to be A4 as opposed to the default US Letter\n\\usepackage{chngpage}\n\\usepackage{fancyhdr} % Required for custom headers\n\\usepackage{extramarks} % Required for headers and footers\n\\usepackage{lastpage} % Required to determine the last page for the footer\n%-----------------1\n\\topmargin=0cm\n\\oddsidemargin=0cm\n\\textheight=22.0cm\n\\textwidth=16cm\n\\parindent=0cm\n\\parskip=0.15cm\n\\topskip=0truecm\n\\raggedbottom\n\\abovedisplayskip=3mm\n\\belowdisplayskip=3mm\n\\abovedisplayshortskip=0mm\n\\belowdisplayshortskip=2mm\n\\normalbaselineskip=12pt\n\\normalbaselines\n\\usepackage{wasysym}\n\\pagestyle{fancy}\n\\lhead{} % Top left header\n\\rhead{Oefententamen} % Top center header\n\\lfoot{Avans Hogeschool Breda} % Bottom left footer\n\\cfoot{} % Bottom center footer\n\\rfoot{Pagina\\ \\thepage} % Bottom right footer\n\\renewcommand\\headrulewidth{0.4pt} % Size of the header rule\n\\renewcommand\\footrulewidth{0.8pt} % Size of the footer rule\n\\usepackage{graphicx} % Required for including pictures\n\\usepackage{pdfpages}\n\\usepackage[font={footnotesize}]{caption}\n\\graphicspath{ {./Images/} } % We can store the generated images in a seperate folder without the hassle\n\\begin{document}\n\\begin{titlepage}\n\\newcommand{\\HRule}{\\rule{\\linewidth}{0.5mm}} % Defines a new command for the horizontal lines, change thickness here\n\n\\center % Center everything on the page\n\nAvans Hogeschool Breda\\\\% Include a department/university logo - this will require the graphicx package\n\\textsc{\\Large Technische Informatica}\\\\[0.5cm] % Major heading such as course name\n\\textsc{\\large Formele Methoden}\\\\[0.5cm] % Minor heading such as course title\n\\HRule \\\\[0.4cm]\n{ \\huge \\bfseries Oefententamen}\\\\[0.4cm] % Title of your document\n\\HRule \\\\[1.5cm]\n\n{\\large \\today}\\\\[3cm] % Date, change the \\today to a set date if you want to be precise\n\n\\vfill % Fill the rest of the page with whitespace\n\n\\end{titlepage}\n\n\\clearpage\n\n\n%------THE CODE BELOW IS AUTOMAGICALLY GENERATED------\n%----Brave wanderers of the latex source beware, there are scary monsters lurking in the dark\n%-----------------------------------------------------\n";
 			return result;
 		}
 
 		private String answersIntro()
 		{
-			String result = "\\chapter{Antwoorden}\n\\setcounter{section}{0}";
+			String result = "\n\\chapter{Antwoorden}\n\\setcounter{section}{0}\n";
 			return result;
 		}
 
 		private String endOfDocument()
 		{
-			String result = "\\end{document}";
+			String result = "\n\\end{document}";
 			return result;
 		}
 
 		private String exerciseNDFAToDFA()
 		{
 			//NDFA_FOR_NDFA_TO_DFA
-			String result = "\\section{NDFA naar DFA}\nZet de onderstaande NDFA om naar een DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
+			String result = "\\section{NDFA naar DFA}\nZet de onderstaande NDFA om naar een DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
@@ -128,7 +128,7 @@ namespace ForGen
 		{
 			String result = "\\section{Reguliere expressie}\nGeef de DFA van de onderstaande reguliere expressie:\\\\\\quote{" +
 				regexStringToLatexString(regex.ToString())
-				+"}\\clearpage";
+				+"}\\clearpage\n";
 			return result;
 		}
 
@@ -136,35 +136,35 @@ namespace ForGen
 		{
 			String result = "\\section{Reguliere expressie}\nGeef de NDFA van de onderstaande reguliere expressie:\\\\\\quote{" +
 				regexStringToLatexString(regex.ToString())
-				+"}\\clearpage";
+				+"}\\clearpage\n";
 			return result;
 		}
 
 		private String exerciseMinimaliseDFA()
 		{
 			//DFA_FOR_MINIMALISE_DFA
-			String result = "\\section{Minimalisatie}\nMinimaliseer de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
+			String result = "\\section{Minimalisatie}\nMinimaliseer de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String exerciseReverseDFA()
 		{
 			//DFA_FOR_DFA_TO_INVERSE_DFA
-			String result = "\\section{Inverse}\nGeef de inverse (het omgekeerde) van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
+			String result = "\\section{Inverse}\nGeef de inverse (het omgekeerde) van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String exerciseDFAToGrammar()
 		{
 			//DFA_FOR_DFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande DFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_DFA_TO_GRAMMAR}\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String exerciseNDFAToGrammar()
 		{
 			//NDFA_FOR_NDFA_TO_GRAMMAR
-			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande NDFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage";
+			String result = "\\section{Grammatica}\nGeef de grammatica van de onderstaande NDFA:\n\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_NDFA_TO_GRAMMAR}\n\n\\clearpage\n";
 			return result;
 		}
 
@@ -185,35 +185,35 @@ namespace ForGen
 		private String answerNDFAToDFA()
 		{
 			//DFA_FOR_NDFA_TO_DFA
-			String result = "\\section{NDFA naar DFA}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_NDFA_TO_DFA}\n\n\\clearpage";
+			String result = "\\section{NDFA naar DFA}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_NDFA_TO_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String answerRegexToDFA()
 		{
 			//DFA_FOR_REGEX_TO_DFA
-			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage";
+			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {DFA_FOR_REGEX_TO_DFA}\\clearpage\n";
 			return result;
 		}
 
 		private String answerRegexToNDFA()
 		{
 			//DFA_FOR_REGEX_TO_DFA
-			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_REGEX_TO_NDFA}\\clearpage";
+			String result = "\\section{Reguliere expressie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {NDFA_FOR_REGEX_TO_NDFA}\\clearpage\n%                                  /   \\       \n% _                        )      ((   ))     (\n%(@)                      /|\\      ))_((     /|\\\n%|-|                     / | \\    (/\\|/\\)   / | \\                      (@)\n%| | -------------------/--|-voV---\\`|'/--Vov-|--\\---------------------|-|\n%|-|                         '^`   (o o)  '^`                          | |\n%| |                               `\\Y/'                               |-|\n%|-|   Hey there                                                       | |\n%| |          I hope you are enjoying our software ;)                  |-|\n%|-|                                             Have a great day!     | |\n%| |                                                           @Guube  |-|\n%|_|___________________________________________________________________| |\n%(@)              l   /\\ /         ( (       \\ /\\   l                `\\|-|\n%                 l /   V           \\ \\       V   \\ l                  (@)\n%                 l/                _) )_          \\I\n%                                   `\\ /'\n%                                     ` \n";
 			return result;
 		}
 
 		private String answerMinimaliseDFA()
 		{
 			//MINIMALISED_DFA_FOR_MINIMALISE_DFA
-			String result = "\\section{Minimalisatie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {MINIMALISED_DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage";
+			String result = "\\section{Minimalisatie}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {MINIMALISED_DFA_FOR_MINIMALISE_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String answerReverseDFA()
 		{
 			//REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA
-			String result = "\\section{Inverse}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage";
+			String result = "\\section{Inverse}\n\\includegraphics[width=\\textwidth,height=0.8\\textheight,keepaspectratio] {REVERSED_DFA_FOR_DFA_TO_INVERSE_DFA}\n\n\\clearpage\n";
 			return result;
 		}
 
@@ -221,7 +221,7 @@ namespace ForGen
 		{
 			String result = "\\section{Grammatica}\n" +
 				regexStringToLatexString((automaton.getGrammar().toBeautifulLatexString()))
-				+"\n\n\\clearpage";
+				+"\n\n\\clearpage\n";
 			return result;
 		}
 
@@ -229,21 +229,21 @@ namespace ForGen
 		{
 			String result = "\\section{Grammatica}\n" +
 				regexStringToLatexString((automaton.getGrammar().toBeautifulLatexString()))
-				+"\n\n\\clearpage";
+				+"\n\n\\clearpage\n";
 			return result;
 		}
 
 		private String answerDFAToRegex(RegularExpression regex)
 		{
 			String result = "\\section{Omzetten naar reguliere expressie}\n\\quote{"+
-				regexStringToLatexString(regex.ToString())+"}\\clearpage";
+				regexStringToLatexString(regex.ToString())+"}\\clearpage\n";
 			return result;
 		}
 
 		private String answerNDFAToRegex(RegularExpression regex)
 		{
 			String result = "\\section{Omzetten naar reguliere expressie}\n\\quote{"+
-				regexStringToLatexString(regex.ToString())+"}\\clearpage";
+				regexStringToLatexString(regex.ToString())+"}\\clearpage\n";
 			return result;
 		}
 
